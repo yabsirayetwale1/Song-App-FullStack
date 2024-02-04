6893e548d5a978b3c384336a8aa71a124694c2b4
yabsirayetwale1 <158767746+yabsirayetwale1@users.noreply.github.com>
2024-02-04 03:15:38 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/songs', postRoutes);
+app.use(postRoutes);
 
 const PORT = process.env.PORT|| 5000;
 // const CONNECTION_URL = 'mongodb+srv://yabsira:yabsira@cluster0.uvc1l0s.mongodb.net/';
